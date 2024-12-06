95a13e9c62c802d1c34e215f2c6cc28c27df49bc
MC-Chiang-777 <chiang.mc0413@gmail.com>
2024-12-07 01:14:59 +0800

    Create MainCharacter.cpp

---
+++ MainCharacter.cpp
@@ -0,0 +1,26 @@
+#include "MainCharacter.h"
+
+void MainCharacter::addATK(int value){
+    atk += value;
+}
+
+void MainCharacter::addDEF(int value){
+    def += value;
+}
+
+void MainCharacter::addSPD(int value){
+    spd += value;
+}
+
+void MainCharacter::addLUK(int value){
+    luk += value;
+}
+
+void MainCharacter::addSPLimit(int value){
+    spLimit += value;
+}
+
+void MainCharacter::addMNY(int value){
+    mny += value;
+}
+
