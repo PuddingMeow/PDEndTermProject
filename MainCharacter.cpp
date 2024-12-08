7308c2636ac6d008aef02210a188f845de48e8d7
MC-Chiang-777 <chiang.mc0413@gmail.com>
2024-12-09 00:35:11 +0800

    Update MainCharacter.cpp

--- MainCharacter.cpp
+++ MainCharacter.cpp
@@ -1,5 +1,17 @@
 #include "MainCharacter.h"
 
+MainCharacter::MainCharacter(const MainCharacter& another){
+    this->name = another.name;
+    this->atk = another.atk;
+    this->def = another.def;
+    this->spd = another.spd;
+    this->luk = another.luk;
+    this->spLimit = another.spLimit;
+    this->skl = another.skl;
+    this->mny = another.mny;
+    this->partner = another.partner;
+}
+
 void MainCharacter::setName(string newName){
     name = newName;
 }
