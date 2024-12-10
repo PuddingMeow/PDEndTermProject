0b1afc7ca31bd44066beacd042c2c7f70470959d
times1-chang <timesone@computer>
2024-12-10 21:15:34 +0800

    add comment

--- battleSimulationFunc.h
+++ battleSimulationFunc.h
@@ -8,6 +8,7 @@ using namespace std;
 
 void delay_ms(std::chrono::milliseconds);
 
+//true player Win , false player Lose
 bool battleSimulation(MainCharacter& player, MainCharacter& partner, MainCharacter& opponent1, MainCharacter& opponent2);
 
 #endif
